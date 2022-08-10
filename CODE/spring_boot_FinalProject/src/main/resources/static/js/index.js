7d1116f0a47f31a7be455f3025f7fa7fff5e9ee0
mshwang223 <mshwang223@gmail.com>
2022-08-10 14:43:18 +0900

    Jaeyun Commit!

--- CODE/spring_boot_FinalProject/src/main/resources/static/js/index.js
+++ CODE/spring_boot_FinalProject/src/main/resources/static/js/index.js
@@ -1,48 +1,72 @@
-
-
 $(document).ready(function(){
-	var interval = setInterval(mainSlide,8000);
-	
-	var movedIndex = 0;
+	var interval = setInterval(mainSlide,5000);
 	
 	// 각 컨트롤 버튼에 대해 처리
 	$('.slide_bar').each(function(index) {
-		$(this).hover(
-			function() {
-				$(this).css('background-color', 'black');
-			},
-			function() {
-				$(this).css('background-color', '#ddd');
-			}
-		);
-		
 		// 클릭했을 때 인덱스값을 moveSlide() 함수에게 전달
 		$(this).on('click', function() {
-			$("#slideBox").finish();
 			clearInterval(interval);
-			moveSlide(index);	
+			
+			moveSlide(chkIdx, index);
+			
+			$(this).css('background-color', 'black');
+			
+			$('.slide_bar_box').find('span').not(this).css('background-color', '#ddd');
+			
+			interval = setInterval(mainSlide,8000);
+
 		});
-
 	});
-	
 
-
+	// 전역 변수 초기값, 애니메이션 실행 시 해당 위치 표시
+	var index = 0;
+	var chkIdx = 0;
+	var maxLength = $(".promotion_area").length;
+	$(".slide_bar_box .slide_bar").eq(index).css('background-color', 'black');	
 	function mainSlide(){
-	  $("#slideBox").animate({"margin-left" : "-1580px"}, 1000, function(){
-	    $("#slideBox").css({"margin-left" : "-8px"});
-	    $(".promotion_area:first-child").insertAfter(".promotion_area:last-child");
-	    
-	    //밑에 동그라미도 같이 적용, 동그라미 클릭 이동
-	  });
+		$("#slideBox").animate({"margin-left" : "-1580px"}, 'slow', function(){
+	    	$("#slideBox").css({"margin-left" : "-8px"});
+	    	$(".promotion_area:first-child").insertAfter(".promotion_area:last-child");
+	    	
+	    	// 해당 하위 태그에 색상으로 표시하고 증가 
+	    	$(".slide_bar_box .slide_bar").eq(index).css('background-color', '#ddd');
+	    	index++;
+	    	
+		    if(index == maxLength) {
+		    	index = 0;
+		    	var slideEqIdx = $(".slide_bar_box .slide_bar").eq(index);
+		    	
+		    	slideEqIdx.css('background-color', 'black');
+		    	$('.slide_bar_box').find('span').not(slideEqIdx).css('background-color', '#ddd');
+		    } else {
+		    	var slideEqIdx = $(".slide_bar_box .slide_bar").eq(index);
+		    	
+		    	slideEqIdx.css('background-color', 'black');
+		    	$('.slide_bar_box').find('span').not(slideEqIdx).css('background-color', '#ddd');	
+		    }
+		    
+		    // index값 전역변수로 적용 마우스 클릭 시 해당 값 비교
+		    chkIdx = index;
+	    });
+	  	
 	};
 
 	// 슬라이드 패널을 움직이는 함수
-	function moveSlide(index) {
-		// 전달 받은 index 값을 movedIndex에 저장
-		movedIndex = index;
-		
+	function moveSlide(chkIdx, index) {
+		var maxLeft = maxLength * 1580;
+		console.log(chkIdx + ", " + index);
 		// 슬라이드 이동
-		var moveLeft = -(index * 1580);		// 왼쪽으로 이동 거리
-		$('#slideBox').animate({'left':moveLeft}, 'slow');
+		if(chkIdx > index) {
+			index = chkIdx - index;
+			var moveLeft = -(maxLeft - (index * 1580));	
+			$('#slideBox').animate({'margin-left':moveLeft}, 'slow');
+		} else if(chkIdx < index) {
+			index = index - chkIdx;
+			var moveLeft = -(index * 1580);
+			$('#slideBox').animate({'margin-left':moveLeft}, 'slow');
+		} else {
+			var moveLeft = -(maxLeft - (1580 * maxLength));	
+			$('#slideBox').animate({'margin-left':moveLeft}, 'slow');	
+		}
 	}
 });	// document.ready
