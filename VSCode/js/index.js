2272aa171805b4c50f1d2711d564422678ff2390
ddowoogi <donguk348@gmail.com>
2022-08-12 11:24:00 +0900

    donguk_update

--- VSCode/js/index.js
+++ VSCode/js/index.js
@@ -80,3 +80,26 @@ $(document).ready(function(){
 		location.href = "/signup";
 	});
 });	// document.ready
+
+const login_btn = document.querySelector(".loginForm");
+const dropdown_login_box = document.querySelector("#dropdown-login");
+
+handleClickLoginBtn = () => {
+  dropdown_login_box.classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  const cond1 = String(event.path[1].id) === 'dropdown-login';
+  const cond2 = String(event.path[1].id) === 'login-form';
+  const clickedInsideLoginForm = cond1 || cond2;
+  if (clickedInsideLoginForm) {
+    return;
+  }
+  if (!event.target.matches('.loginForm')) {
+    if (dropdown_login_box.classList.contains('show')) {
+      dropdown_login_box.classList.remove('show');
+    }
+  }
+}
+
+login_btn.addEventListener("click", handleClickLoginBtn);
