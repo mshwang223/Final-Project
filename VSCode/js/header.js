2949fc966dfe51b1a67629b5247c29c25798c979
kyeogrok <kyeongrok@naver.com>
2022-08-17 02:45:53 +0900

    update_header

--- VSCode/js/header.js
+++ VSCode/js/header.js
@@ -1,12 +1,41 @@
 /********************************
  * 파일명 : header.js
- * 용 도 : 메인 페이지 slide show 이벤트 
- * 작성자 : 황재윤
+ * 용 도 : 메인 페이지 header 로그인 회원가입 
+ * 작성자 : 황재윤, 이동욱
 *********************************/
 
-$(document).ready(function(){ 
-	// 클릭 시 회원가입 페이지 이동
-	$(".login_box span:nth-child(2)").on('click', function() {
-		location.href = "/signup";
+$(document).ready(function(){
+	
+	// 로그인 드롭다운 확인
+	var chk = 0;
+	
+	$(".loginForm").on('click', function() {
+		$("#dropdown-login").slideToggle(function() {
+			if($(this).is(':hidden')){	// 닫힐 때
+				chk = 1;
+			} else {
+				chk = 2;
+			}
+		});
 	});
-});	
+	
+	// 외부 클릭 시 드롭다운 박스 히든
+	window.onclick = function(event) {
+		const pathId = event.path[1].id;
+		var divArray = new Array("dropdown-login", "login-form", "input-div", "forgotInfo");
+		/*
+		const cond1 = event.path[1].id == String("dropdown-login");
+		const cond2 = event.path[1].id == String("login-form");
+		const clickedInsideLoginForm = cond1 || cond2;
+		*/
+		for(i in divArray) {
+			if(pathId === divArray[i] ) return false;
+		}
+		
+		if (!event.target.matches('.loginForm')) {
+			if (chk == 2) $("#dropdown-login").hide();
+		}
+	}
+});
+
+	
