f6632bda0b6428cda8a5f7145e30ab10fedd24ca
Hyewon <hwwwon1027@gmail.com>
2022-08-18 09:36:26 +0900

    index page desigin

--- VSCode/js/index.js
+++ VSCode/js/index.js
@@ -105,4 +105,18 @@ window.onclick = function (event) {
 login_btn.addEventListener("click", handleClickLoginBtn);
 
 
-  
+// 자세히보기 버튼 이동
+let slideBtn = document.getElementsByClassName("slide_btn");
+let partyArea = document.getElementById("partyArea");
+let photoArea = document.getElementById("photoArea");
+let itemArea = document.getElementById("itemArea");
+let playArea = document.getElementById("playArea");
+console.log(slideBtn);
+
+for(let i = 0; i < slideBtn.length; i++){
+	slideBtn[i].addEventListener('click',() => {
+		if(i == 0){
+			
+		}
+	});
+}
