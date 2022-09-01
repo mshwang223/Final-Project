168ff1cab89d42d7ebd6771b4fde2a33f220b83b
Hyewon <hwwwon1027@gmail.com>
2022-09-01 23:16:32 +0900

    remove alert

--- CODE/spring_boot_FinalProject/src/main/resources/static/js/insertHotel.js
+++ CODE/spring_boot_FinalProject/src/main/resources/static/js/insertHotel.js
@@ -77,7 +77,6 @@ $(document).ready(function(){
           processData : false,
           success:function(result){
             // 성공 시 결과 받음
-            alert(result);
             if(result){
                alert("등록되었습니다.");
                location.href = "/adminInsertHotel";
