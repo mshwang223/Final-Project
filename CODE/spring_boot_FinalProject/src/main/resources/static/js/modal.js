a35efad579b5718437871a386eb726e2e77fcfa8
mshwang223 <mshwang223@gmail.com>
2022-09-13 17:49:00 +0900

    Jaeyun Commit!

--- CODE/spring_boot_FinalProject/src/main/resources/static/js/modal.js
+++ CODE/spring_boot_FinalProject/src/main/resources/static/js/modal.js
@@ -2,12 +2,14 @@ document.addEventListener("DOMContentLoaded", function(){
 	const modals = document.querySelectorAll(".modal");
 	const findIdModal = document.querySelector("#findIdModal");
 	const findPwModal = document.querySelector("#findPwModal");
+	const dropdownLogin = document.querySelector("#dropdown-login");
 	const findIdBtn = document.querySelector("#findIdBtn");
 	const findPwBtn = document.querySelector("#findPwBtn");
 	const closeBtns = document.querySelectorAll(".close");
 	
 	if(findIdBtn != null) {
 		findIdBtn.onclick = function() {
+			dropdownLogin.style.display = "none";
 			findPwModal.style.display = "none";
 			findIdModal.style.display = "block";
 		}
@@ -15,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function(){
 	
 	if(findPwBtn != null) {
 		findPwBtn.onclick = function () {
+			dropdownLogin.style.display = "none";
 			findIdModal.style.display = "none";
 			findPwModal.style.display = "block";
 		}
